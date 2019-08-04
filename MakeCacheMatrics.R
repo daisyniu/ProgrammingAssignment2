9dcc143fcd9fde6d24c740562bff3a692be0433e
daisyniu <52614203+daisyniu@users.noreply.github.com>
2019-08-04 08:47:02 -0400

    Update MakeCacheMatrics.R

--- MakeCacheMatrics.R
+++ MakeCacheMatrics.R
@@ -1,4 +1,7 @@
-makeCacheMatix <- function(x) {
+
+##The function is to set the data, get the data, set the inverse of the matrix and get the inverse of the matrix
+## And also set the cache pointer to Null if new data is set
+makeCacheMatix <- function(x=matrix()) {
         xinverse <- NULL
         set <- function(y) {
                 x <<- y
